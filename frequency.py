45ced2585d7db87687d0ba6c731d663d2d0e9fa0
venegu <venegu@gmail.com>
2014-11-13 18:36:55 -0500

    Fixed easy way freq letters

--- frequency.py
+++ frequency.py
@@ -1,8 +1,9 @@
-phrase = raw_input('')
+phrase = raw_input('Please enter a random phrase:')
 print phrase 
 #easy way to count letters 
 import collections
 letters = collections.Counter(phrase) 
+print letters 
 
 #Making a function to count letters
 
