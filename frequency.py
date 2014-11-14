5422c69ccc5cb111ca264e5b09cd3746574fd691
venegu <venegu@gmail.com>
2014-11-13 19:42:41 -0500

    Added easy word count

--- frequency.py
+++ frequency.py
@@ -1,9 +1,31 @@
+#Taking input from the User
 phrase = raw_input('Please enter a random phrase:')
 print phrase 
-#easy way to count letters 
+
+#Easy Way -- With Collections 
+#Count Letters 
 import collections
 letters = collections.Counter(phrase) 
-print letters 
 
-#Making a function to count letters
+#This will print out a dictionary of letter and their freq
+print letters
 
+#Counting Words 
+#Splits the raw_input and puts into list
+listWords = phrase.split()
+
+#This will print a list of the words from raw_input
+print listWords 
+
+#Will sort word into Dictionary with frequency
+words = collections.Counter(listWords)
+
+#This will print out a dictionary of words and their freq
+print words
+
+#this method will include punctuation as part of a word
+#if input "bananas! bananas" 
+#will output 1 for bananas! and 1 for banana
+
+#More throught out way -- Making a function
+#Counting Letters
